ffcb0eecb6cae3ed423abc215eb28f4c349b6b27
Tapas Ranjan Nayak <tapasrnk@gmail.com>
2023-10-12 22:30:01 +0530

    made messageQueue lazysingleton

--- src/main/java/messageQueue/MessageQueue.java
+++ src/main/java/messageQueue/MessageQueue.java
@@ -1,8 +1,15 @@
 package messageQueue;
 
 public class MessageQueue implements IMessageQueue{
+    private static MessageQueue instance;
     private ITopic topics;
-
+    private MessageQueue() {}
+    public static MessageQueue getInstance() {
+        if (instance == null) {
+            instance = new MessageQueue();
+        }
+        return instance;
+    }
     @Override
     public String addTopic(Integer numberOfPartitions) {
         return null;
